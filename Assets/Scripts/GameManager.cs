cb91b73ca9bcca12046c52b0ec236573e47e89ad
eju1121 <shdnr0506@naver.com>
2024-07-19 16:47:47 +0900

    7/19 과제
    게임매니저 골드 스크립트 오류, 레드 그린 슬라임 추가, 애니메이션 추가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,17 +9,19 @@ public class GameManager : MonoBehaviour
     [SerializeField] private float damage; //초기 설정 데미지
     [SerializeField] private int percent; //크리티컬 기준
     private Monster curSlime;
+    private Gold changeGold;
     private float curDamage; //초기 데미지 저장용
     [SerializeField] private float criticalDamage;
     private int criticalPrecent;
-    private float Gold; //처치 시 랜덤 골드량
-    private float curGold; //현재 보유 골드량
+    private float gold;
+    private float curGold;
 
     public void SpawnSlime()
     {
         int spawnIndex = UnityEngine.Random.Range(0, slimes.Length);
         GameObject newSlime = Instantiate(slimes[spawnIndex].gameObject);
         curSlime=newSlime.GetComponent<Monster>();
+
     }
 
     public void CheckCritical()
@@ -34,22 +36,20 @@ public class GameManager : MonoBehaviour
         }  
     }
 
-    public void GetGold()
+    private void GetGold()
     {
-        if (curSlime == null)
-        {
-            Gold = Random.Range(1, 101);
-            curGold += Gold;
-            
-        }
-    }
+        gold = Random.Range(1, 101);
+        curGold += gold;
 
+        changeGold.ChangeGoldUI(curGold);
+    }
 
     private void Update()
     {
         if (curSlime == null)
         {
             SpawnSlime();
+            GetGold();
         }
     }
 
--- Assets/Scripts/Gold.cs
+++ Assets/Scripts/Gold.cs
@@ -1,17 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Gold : MonoBehaviour
+public class Gold : MonoBehaviour //골드 텍스트만 바꿔주는 역할, 현재 골드량을 받아올 곳이 있어야함
 {
-    private GameManager randomGold;
-    private int dropGold;
-    private int curGold;
+    private float gold;
+    public float curGold;
+    [SerializeField] private Text goldText;
 
 
-    private void ChangeGoldUI()
+    public void ChangeGoldUI(float gold)
     {
-        randomGold.GetGold();
+        goldText.text = (gold.ToString() + "원");
     }
     
 }
